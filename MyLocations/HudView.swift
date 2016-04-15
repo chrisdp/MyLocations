e0b50e0e44b51831a1d1a94b2f5b19fbb0537502
chrisdp <chrisdwyerperkins@gmail.com>
2016-04-14 22:31:39 -0300

    HUD now uses checkmark image and can have its text set and displayed.

--- MyLocations/HudView.swift
+++ MyLocations/HudView.swift
@@ -25,11 +25,39 @@ class HudView: UIView {
     let boxWidth: CGFloat = 96
     let boxHeight: CGFloat = 96
     
-    let boxRect = CGRect(x: round((bounds.size.width - boxWidth) / 2), y: round((bounds.size.height - boxHeight) / 2), width: boxWidth, height: boxHeight)
+    let boxRect = CGRect(
+      x: round((bounds.size.width - boxWidth) / 2),
+      y: round((bounds.size.height - boxHeight) / 2),
+      width: boxWidth,
+      height: boxHeight
+    )
     
     let roundedRect = UIBezierPath(roundedRect: boxRect, cornerRadius: 10)
     
     UIColor(white: 0.3, alpha: 0.8).setFill()
     roundedRect.fill()
+    
+    if let image = UIImage(named: "Checkmark") {
+      let imagePoint = CGPoint(
+        x: center.x - round(image.size.width / 2),
+        y: center.y - round(image.size.height / 2)
+      )
+      
+      image.drawAtPoint(imagePoint)
+    }
+    print(text)
+    let attribs = [
+      NSFontAttributeName: UIFont.systemFontOfSize(16),
+      NSForegroundColorAttributeName: UIColor.whiteColor()
+    ]
+    
+    let textSize = text.sizeWithAttributes(attribs)
+    
+    let textPoint = CGPoint(
+      x: center.x - round(textSize.width / 2),
+      y: center.y - round(textSize.height / 2) + boxHeight / 4
+    )
+    
+    text.drawAtPoint(textPoint, withAttributes: attribs)
   }
 }
